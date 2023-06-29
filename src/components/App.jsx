a7ba35c7c27f44b6960ebcdc55eca8bd4c5698a0
Krisztina Csanyi <krisztina.csanyi@case-solvers.com>
2023-06-29 19:17:54 +0200

    Remake header in a different style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,25 @@
 import React from 'react'
 import Header from './Header'
 import WebFont from 'webfontloader';
+import '../assets/css/app.css'
 function App() {
     WebFont.load({
         google: {
-            families: ['Lato']
+            families: ['Oswald', 'Source Sans Pro', 'Raleway']
         }
     });
     return (
         <div className="app">
             <Header />
+            <div id="parallax-text">
+                <section>
+                    <div className="block">
+                        <p><span className="first-character">I</span>n 1978, Brian Smith landed in Southern California with a bag of sheepskin boots and hope. He fell in love with the sheepskin experience and was convinced the world would one day share this love. The beaches of Southern California had long been an epicenter of a relaxed, casual lifestyle, a lifestyle that Brian felt was a perfect fit for his brand. So he founded the UGG brand, began selling his sheepskin boots and they were an immediate sensation. By the mid 1980's, the UGG brand became a symbol of relaxed southern California culture, gaining momentum through surf shops and other shops up and down the coast of California, from San Diego to Santa Cruz. UGG boots reached beyond the beach, popping up in big cities and small towns all over, and in every level of society. Girls wore their surfer boyfriend's pair of UGG boots like a letterman jacket. When winter came along, UGG boots were in ski shops and were seen in lodges from Mammoth to Aspen.</p>
+                        <p className="line-break margin-top-10"></p>
+                        <p className="margin-top-10">The UGG brand began to symbolize those who embraced sport and a relaxed, active lifestyle. More than that, an emotional connection and a true feeling of love began to grow for UGG boots, just as Brian had envisioned. People didn't just like wearing UGG boots, they fell in love with them and literally could not take them off. By the end of the 90's, celebrities and those in the fashion world took notice of the UGG brand. A cultural shift occurred as well - people were embracing, and feeling empowered, by living a more casual lifestyle and UGG became one of the symbols of this lifestyle. By 2000, a love that began on the beaches had become an icon of casual style. It was at this time that the love for UGG grew in the east, over the Rockies and in Chicago. In 2000, UGG Sheepskin boots were first featured on Oprah's Favorite Things® and Oprah emphatically declared that she "LOOOOOVES her UGG boots." From that point on, the world began to notice.</p>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,24 +3,20 @@ import '../assets/css/header.css'
 
 export default function Header() {
     return (
-        <section class="header">
-            <div class="inner-header flex">
+        <header className='light'>
+            <menu>
+                <ul>
+                    <li><span>Welcome</span></li>
+                    <li><span>About Us</span></li>
+                    <li><span>What we do</span></li>
+                    <li><span>Our portfolio</span></li>
+                    <li><span>Contact us</span></li>
+                </ul>
+            </menu>
+            <section>
                 <h1>Krisztina Csanyi</h1>
-            </div>
-            <div className="waves">
-                <svg class="waves" xmlns="http://www.w3.org/2000/svg" xlink="http://www.w3.org/1999/xlink"
-                    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-                    <defs>
-                        <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-                    </defs>
-                    <g class="parallax">
-                        <use href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)" />
-                        <use  href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
-                        <use href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
-                        <use href="#gentle-wave" x="48" y="7" fill="#fff" />
-                    </g>
-                </svg>
-            </div>
-        </section>
+                <h2>Frontend developer</h2>
+            </section>
+        </header>
     )
 }
