b4431fafae176465438dbd8b0cd650515efb1d89
Krisztina Csanyi <krisztina.csanyi@case-solvers.com>
2023-06-30 14:40:24 +0200

    Add smooth scroll

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,10 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault()
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+window.addEventListener('load', () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault()
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            })
         })
     })
 })
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,21 +2,25 @@ import React from "react"
 import Header from "./Header"
 import Objective from "./Objective"
 import WebFont from "webfontloader"
-import "../assets/js/app"
+import SmoothScroll from "smooth-scroll"
 import "../assets/css/app.css"
 
 function App() {
-  WebFont.load({
-    google: {
-      families: ["Oswald", "Source Sans Pro", "Raleway"],
-    },
-  })
-  return (
-    <div className="app">
-      <Header />
-      <Objective />
-    </div>
-  )
+    new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true
+    })
+    WebFont.load({
+        google: {
+            families: ["Oswald", "Source Sans Pro", "Raleway"],
+        },
+    })
+    return (
+        <div className="app">
+            <Header />
+            <Objective />
+        </div>
+    )
 }
 
 export default App
