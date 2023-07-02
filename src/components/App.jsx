072913ed9cf3234d64eb7cd59e17d75d8f9ab0bb
Tina <krisztina.csanyi@proton.me>
2023-07-02 11:39:42 +0200

    Finish the projects section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,55 @@
-import React from "react"
-import Header from "./Header"
-import Objective from "./Objective"
-import WebFont from "webfontloader"
-import SmoothScroll from "smooth-scroll"
-import "../assets/js/app"
-import "../assets/css/app.css"
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Objective from "./Objective";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import WebFont from "webfontloader";
+import SmoothScroll from "smooth-scroll";
+import Parallax from "parallax-js";
+import "../assets/js/app";
+import "../assets/css/app.css";
 
 function App() {
-    new SmoothScroll('a[href*="#"]', {
-        speed: 500,
-        speedAsDuration: true
-    })
-    WebFont.load({
-        google: {
-            families: ["Oswald", "Source Sans Pro"],
-        },
-    })
-    return (
-        <div className="app">
-            <Header />
-            <Objective />
+  new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true,
+  });
+  WebFont.load({
+    google: {
+      families: ["Oswald", "Source Sans Pro"],
+    },
+  });
+  useEffect(() => {
+    new Parallax(document.getElementById("js-scene"));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Objective />
+      <section className="js-scene reveal">
+        <div className="items__inner" id="js-scene">
+          <div className="items__layer layer" data-depth="0.85">
+            <div className="items__item" data-title="no.1"></div>
+          </div>
+          <div className="items__layer layer" data-depth="0.70">
+            <div className="items__item" data-title="no.2"></div>
+          </div>
+          <div className="items__layer layer" data-depth="0.65">
+            <div className="items__item" data-title="no.3"></div>
+          </div>
+          <div className="items__layer layer" data-depth="1.20">
+            <div className="items__item" data-title="no.4"></div>
+          </div>
+          <div className="items__layer layer" data-depth="0.00">
+            <div className="items__item" data-title="no.5"></div>
+          </div>
         </div>
-    )
+      </section>
+      <Projects />
+      <Contact />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import "../assets/css/header.css";
 
 export default function Header() {
-    return (
-        <header className="light">
-            <div className="header-inner">
-                <menu>
-                    <a href="#objective">Objective</a>
-                    <a href="#projects">Projects</a>
-                    <a href="#skills">Skills</a>
-                    <a href="#contact">Contact</a>
-                </menu>
-                <section className="loadTop">
-                    <h1>Krisztina Csanyi</h1>
-                    <h2>Frontend developer</h2>
-                </section>
-            </div>
-        </header>
-    );
+  return (
+    <header className="light">
+      <div className="header-inner">
+        <menu>
+          <a href="#objective">Objective</a>
+          <a href="#projects">Projects</a>
+          <a href="#experience">Experience</a>
+          <a href="#skills">Skills</a>
+          <a href="#contact">Contact</a>
+        </menu>
+        <section className="loadTop">
+          <h1>Krisztina Csanyi</h1>
+          <h2>Frontend developer</h2>
+        </section>
+      </div>
+    </header>
+  );
 }
