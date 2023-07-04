9bf953fc373ecec9dc26cfa3a9ee756774df68de
Krisztina Csanyi <krisztina.csanyi@case-solvers.com>
2023-07-04 11:55:43 +0200

    Change text

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,54 +2,55 @@ import React from "react";
 import "../assets/css/skills.css";
 
 export default function Skills() {
-  return (
-    <section id="skills">
-      <div className="title reveal">Skills</div>
-      <div className="skill-container reveal">
-        <div className="skill">
-          <span>HTML5</span>
-        </div>
-        <div className="skill">
-          <span>CSS3</span>
-        </div>
-        <div className="skill">
-          <span>JavaScript</span>
-        </div>
-        <div className="skill">
-          <span>Jquery</span>
-        </div>
-        <div className="skill">
-          <span>PHP</span>
-        </div>
-        <div className="skill">
-          <span>SQL</span>
-        </div>
-        <div className="skill">
-          <span>Reactjs</span>
-        </div>
-        <div className="skill">
-          <span>Nodejs</span>
-        </div>
-        <div className="skill">
-          <span>Jira</span>
-        </div>
-        <div className="skill">
-          <span>Git</span>
-        </div>
-        <div className="skill">
-          <span>Responsive</span>
-        </div>
-        <div className="skill">
-          <span>English C1</span>
-        </div>
-      </div>
-      <div className="image">
-        <div className="image-text">
-          Even after obtaining all these skills, I will never feel satisfied
-          enough. I have a great desire to learn everything and have broad
-          knowledge in every area of web development.
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="skills">
+            <div className="title reveal">Skills</div>
+            <div className="skill-container reveal">
+                <div className="skill">
+                    <span>HTML5</span>
+                </div>
+                <div className="skill">
+                    <span>CSS3</span>
+                </div>
+                <div className="skill">
+                    <span>JavaScript</span>
+                </div>
+                <div className="skill">
+                    <span>Jquery</span>
+                </div>
+                <div className="skill">
+                    <span>PHP</span>
+                </div>
+                <div className="skill">
+                    <span>SQL</span>
+                </div>
+                <div className="skill">
+                    <span>Reactjs</span>
+                </div>
+                <div className="skill">
+                    <span>Nodejs</span>
+                </div>
+                <div className="skill">
+                    <span>Jira</span>
+                </div>
+                <div className="skill">
+                    <span>Git</span>
+                </div>
+                <div className="skill">
+                    <span>Responsive</span>
+                </div>
+                <div className="skill">
+                    <span>English C1</span>
+                </div>
+            </div>
+            <div className="image">
+                <div className="image-text">
+                    Even after obtaining all these skills, I never feel satisfied
+                    enough. I have a deep desire to learn everything and have broad
+                    knowledge in every area of web development,
+                    and I have been working towards this goal for the past few years.
+                </div>
+            </div>
+        </section>
+    );
 }
