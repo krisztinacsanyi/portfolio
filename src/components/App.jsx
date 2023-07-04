6bef388dedd31bde49941973cf52ce5672b95001
Krisztina Csanyi <krisztina.csanyi@case-solvers.com>
2023-07-04 16:23:24 +0200

    Fix some lighthouse errors and notices

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,62 @@
-import React, { useEffect } from "react";
-import Header from "./Header";
-import Objective from "./Objective";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import WebFont from "webfontloader";
-import SmoothScroll from "smooth-scroll";
-import Parallax from "parallax-js";
-import "../assets/js/app";
 import "../assets/css/app.css";
-import Experience from "./Experience";
+import "../assets/js/app";
 import Contact from "./Contact";
+import Experience from "./Experience";
+import Header from "./Header";
+import lozad from 'lozad'
+import Objective from "./Objective";
+import Parallax from "parallax-js";
+import Projects from "./Projects";
+import React, { useEffect } from "react";
+import Skills from "./Skills";
+import SmoothScroll from "smooth-scroll";
+import WebFont from "webfontloader";
 
 function App() {
-  new SmoothScroll('a[href*="#"]', {
-    speed: 500,
-    speedAsDuration: true,
-  });
-  WebFont.load({
-    google: {
-      families: ["Oswald", "Source Sans Pro"],
-    },
-  });
-  useEffect(() => {
-    new Parallax(document.getElementById("js-scene"));
-  }, []);
+    new SmoothScroll('a[href*="#"]', {
+        speed: 500,
+        speedAsDuration: true,
+    });
+    WebFont.load({
+        google: {
+            families: ["Oswald", "Source Sans Pro"],
+        },
+    });
+    useEffect(() => {
+        new Parallax(document.getElementById("js-scene"));
+    }, []);
+    const observer = lozad();
+    observer.observe();
 
-  return (
-    <div className="app">
-      <Header />
-      <Objective />
-      <section className="js-scene reveal">
-        <div className="items__inner" id="js-scene">
-          <div className="items__layer layer" data-depth="0.85">
-            <div className="items__item" data-title="no.1"></div>
-          </div>
-          <div className="items__layer layer" data-depth="0.70">
-            <div className="items__item" data-title="no.2"></div>
-          </div>
-          <div className="items__layer layer" data-depth="0.65">
-            <div className="items__item" data-title="no.3"></div>
-          </div>
-          <div className="items__layer layer" data-depth="1.20">
-            <div className="items__item" data-title="no.4"></div>
-          </div>
-          <div className="items__layer layer" data-depth="0.00">
-            <div className="items__item" data-title="no.5"></div>
-          </div>
+    return (
+        <div className="app">
+            <Header />
+            <Objective />
+            <section className="js-scene reveal">
+                <div className="items__inner" id="js-scene">
+                    <div className="items__layer layer" data-depth="0.85">
+                        <div className="items__item" data-title="no.1"></div>
+                    </div>
+                    <div className="items__layer layer" data-depth="0.70">
+                        <div className="items__item" data-title="no.2"></div>
+                    </div>
+                    <div className="items__layer layer" data-depth="0.65">
+                        <div className="items__item" data-title="no.3"></div>
+                    </div>
+                    <div className="items__layer layer" data-depth="1.20">
+                        <div className="items__item" data-title="no.4"></div>
+                    </div>
+                    <div className="items__layer layer" data-depth="0.00">
+                        <div className="items__item" data-title="no.5"></div>
+                    </div>
+                </div>
+            </section>
+            <Projects />
+            <Skills />
+            <Experience />
+            <Contact />
         </div>
-      </section>
-      <Projects />
-      <Skills />
-      <Experience />
-      <Contact/>
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,68 +3,68 @@ import Exp from "./experience/Exp";
 import "../assets/css/experience.css";
 
 export default function Experience() {
-  let EXP = [
-    {
-      title: "FRONTEND | FULLSTACK DEVELOPER",
-      subtitle: "Case Solvers",
-      content:
-        "As an employee at this company, I served as the sole frontend developer, allowing me to handle every aspect of page creation from inception to completion. This involved conceptualizing the design based on business requirements, establishing database connections, and developing both the backend and frontend components. Additionally, I undertook the crucial responsibilities of SEO management, conducting performance testing and optimization, and adhering to clean code principles. Working closely with my teammates, I collaborated on updating application versions utilizing Git. Furthermore, I embraced valuable learning opportunities within the organization, expanding my skill set with Node.js and React.js.",
-      time: "2020 - now",
-    },
-    {
-      title: "NODE.JS DEVELOPER",
-      subtitle: "Webuni",
-      content:
-        "Gained some knowledge about these topics: JavaScript, V8, Node.Js, npm, Postman, Express.js, Middleware, Request, Response, REST, RESTful APIs.",
-      time: "2022",
-    },
-    {
-      title: "PEGA CERTIFIED SYSTEM ARCHITECT",
-      subtitle: "Pega",
-      content:
-        "Learned the core principles of low-code application development on Pega Platform.",
-      time: "2021",
-    },
-    {
-      title: "WEB DEVELOPER",
-      subtitle: "Webler Oktatóstúdió",
-      content:
-        "During the web programming course, I learned about all forms of website development, from simple static websites to complete web store and responsive websites using: HTML5, CSS3, Bootstrap, Javascript, PHP, MySQL.",
-      time: "2020",
-    },
-    {
-      title: "SYSTEM ADMINISTRATOR | WEB DEVELOPER",
-      subtitle: "Krémpatika",
-      content:
-        "I was responsible for the visual and structural components of the online store, shaping its image and form. I employed my coding skills to make new elements while keeping the overall aesthetic of the shop. Additionally, I was also a system administrator for the webshop, entailing the resolution of bugs, addressing emerging business requirements, and ensuring uninterrupted operation of the store.",
-      time: "2019 - 2020",
-    },
-    {
-      title: "BUSINESS INFORMATICS ENGINEER",
-      subtitle: "Corvinus University of Budapest",
-      content:
-        "During my time at Corvinus I gained practical knowledge in the fields of software development, business analytics (systems engineering), IT project management, process development, database management and development, digitalisation, IT consulting, business intelligence solutions.",
-      time: "2017-2021",
-    },
-  ];
-  return (
-    <section id="experience">
-      <div className="title reveal">Experience and education</div>
-      <div className="timeline reveal">
-        <ul>
-          {EXP.map((element, index) => {
-            return (
-              <Exp
-                title={element.title}
-                subtitle={element.subtitle}
-                content={element.content}
-                time={element.time}
-                key={index}
-              />
-            );
-          })}
-        </ul>
-      </div>
-    </section>
-  );
+    let EXP = [
+        {
+            title: "FRONTEND | FULLSTACK DEVELOPER",
+            subtitle: "Case Solvers",
+            content:
+                "As an employee at this company, I served as the sole frontend developer, allowing me to handle every aspect of page creation from inception to completion. This involved conceptualizing the design based on business requirements, establishing database connections, and developing both the backend and frontend components. Additionally, I undertook the crucial responsibilities of SEO management, conducting performance testing and optimization, and adhering to clean code principles. Working closely with my teammates, I collaborated on updating application versions utilizing Git. Furthermore, I embraced valuable learning opportunities within the organization, expanding my skill set with Node.js and React.js.",
+            time: "2020 - now",
+        },
+        {
+            title: "NODE.JS DEVELOPER",
+            subtitle: "Webuni",
+            content:
+                "Gained some knowledge about these topics: JavaScript, V8, Node.Js, npm, Postman, Express.js, Middleware, Request, Response, REST, RESTful APIs.",
+            time: "2022",
+        },
+        {
+            title: "PEGA CERTIFIED SYSTEM ARCHITECT",
+            subtitle: "Pega",
+            content:
+                "Learned the core principles of low-code application development on Pega Platform.",
+            time: "2021",
+        },
+        {
+            title: "WEB DEVELOPER",
+            subtitle: "Webler Oktatóstúdió",
+            content:
+                "During the web programming course, I learned about all forms of website development, from simple static websites to complete web store and responsive websites using: HTML5, CSS3, Bootstrap, Javascript, PHP, MySQL.",
+            time: "2020",
+        },
+        {
+            title: "SYSTEM ADMINISTRATOR | WEB DEVELOPER",
+            subtitle: "Krémpatika",
+            content:
+                "I was responsible for the visual and structural components of the online store, shaping its image and form. I employed my coding skills to make new elements while keeping the overall aesthetic of the shop. Additionally, I was also a system administrator for the webshop, entailing the resolution of bugs, addressing emerging business requirements, and ensuring uninterrupted operation of the store.",
+            time: "2019 - 2020",
+        },
+        {
+            title: "BUSINESS INFORMATICS ENGINEER",
+            subtitle: "Corvinus University of Budapest",
+            content:
+                "During my time at Corvinus I gained practical knowledge in the fields of software development, business analytics (systems engineering), IT project management, process development, database management and development, digitalisation, IT consulting, business intelligence solutions.",
+            time: "2017-2021",
+        },
+    ];
+    return (
+        <section id="experience">
+            <div className="title reveal">Experience and education</div>
+            <div className="timeline reveal">
+                <ul>
+                    {EXP.map((element, index) => {
+                        return (
+                            <Exp
+                                title={element.title}
+                                subtitle={element.subtitle}
+                                content={element.content}
+                                time={element.time}
+                                key={index}
+                            />
+                        );
+                    })}
+                </ul>
+            </div>
+        </section>
+    );
 }
